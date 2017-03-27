c2e9527c240b3224596433e982ed1d9eef881e3d
Taylor Dawson <tdawson16@georgefox.edu>
2017-03-27 08:06:17 -0700

    Moved the HTTPRequester js to a seperate folder. Added search results (terrible looking for now...)

---
+++ js/elasticSearchRequest.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Foba on 3/27/2017.
+ */
+//TODO: Condense function
+(function() {
+    const ElasticSearchAddr = "http://35.185.217.146:9200/firebase/line/_search?q=text_entry_norm:";
+    var httpRequest;
+    document.getElementById("button").onclick = function() {
+
+        //Todo: Sanitize search text
+        var searchText = document.getElementById('search_text').value;
+        var searchEncoded = encodeURIComponent(searchText);
+
+        // Tilda added for fuzzy searching
+        //Todo: Make a json structured query
+        makeRequest(ElasticSearchAddr + searchText+'~');
+    };
+
+    function makeRequest(url) {
+        httpRequest = new XMLHttpRequest();
+
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    }
+
+    function alertContents() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                //$("#par_text").text(httpRequest.responseText);
+                //TODO: Move outside of function?????
+                var responseJSON = JSON.parse(httpRequest.responseText);
+                displaySearchResults(responseJSON.hits.hits);
+            } else {
+                alert('There was a problem with the request. Status: ' + httpRequest.status);
+            }
+        }
+    }
+})();
+
+function displaySearchResults(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<p>' + arr[i]._source.speaker + ' : ' +
+            arr[i]._source.text_entry_orig + '<\p>';
+    }
+    document.getElementById("search_content").innerHTML = out;
+}
